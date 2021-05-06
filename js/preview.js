f4646a18b980abd9dc05b2a95f640211020090b1
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-05-06 12:03:05 +0200

    Added gsap functionality and images in portfolio

---
+++ js/preview.js
@@ -0,0 +1,60 @@
+const title = document.getElementsByClassName("project-title");
+const tl = gsap.timeline({ paused: true, reversed: true });
+const firstLink = document.getElementById("first-link");
+const secondLink = document.getElementById("second-link");
+const thirdLink = document.getElementById("third-link");
+const fourthLink = document.getElementById("fourth-link");
+const fifthLink = document.getElementById("fifth-link");
+const sixthLink = document.getElementById("sixth-link");
+const imagePreview = document.getElementById("project-image");
+
+let hover = tl.to(".project-image", 0.5, {
+  width: "50%",
+  ease: Power2.easeOut,
+});
+
+for (i = 0; i < title.length; i++) {
+  title[i].addEventListener("mouseenter", () => hover.play());
+  title[i].addEventListener("mouseleave", () => hover.reverse());
+}
+
+firstLink.addEventListener(
+  "mouseover",
+  () => (imagePreview.style.backgroundImage = "url('./media/images/wiam.png')")
+);
+
+secondLink.addEventListener(
+  "mouseover",
+  () =>
+    (imagePreview.style.backgroundImage = "url('./media/images/nordic.png')")
+);
+
+thirdLink.addEventListener(
+  "mouseover",
+  () =>
+    (imagePreview.style.backgroundImage = "url('./media/images/tattoo.png')")
+);
+
+fourthLink.addEventListener(
+  "mouseover",
+  () =>
+    (imagePreview.style.backgroundImage =
+      "url('./media/images/sherocks-home.png')")
+);
+
+fifthLink.addEventListener(
+  "mouseover",
+  () =>
+    (imagePreview.style.backgroundImage = "url('./media/images/karolina.png')")
+);
+
+sixthLink.addEventListener(
+  "mouseover",
+  () => (imagePreview.style.backgroundImage = "url('./media/images/chef.png')")
+);
+
+/*
+document.getElementById("first-link").addEventListener("hover", function () {
+  document.getElementById("project-image").src = "./media/images/create.jpg";
+});
+*/
